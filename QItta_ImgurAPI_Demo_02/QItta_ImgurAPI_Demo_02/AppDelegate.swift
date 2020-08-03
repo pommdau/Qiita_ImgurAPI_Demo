24fbaf8e2371a9cee1969a7c920a11b49083d50a
hikeuchi <hikeuchi@morisawa.co.jp>
2020-08-03 22:07:06 +0900

    create project

---
+++ QItta_ImgurAPI_Demo_02/QItta_ImgurAPI_Demo_02/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QItta_ImgurAPI_Demo_02
+//
+//  Created by HIROKI IKEUCHI on 2020/08/03.
+//  Copyright © 2020 hikeuchi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
